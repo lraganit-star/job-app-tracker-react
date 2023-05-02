4d25b7a230ab1b7c2c3e228ba6dd43415ccabbc8
Leslie Raganit <niki@Leslies-MacBook-Pro.local>
2023-05-02 00:50:21 -0700

    card UI skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,34 @@
 import { useState } from "react";
 import "./App.css";
 
-export default function Card() {
-  // const [companyName, setCompanyName] = useState("");
-  // const [jobTitle, setJobTitle] = useState("");
-  // const [location, setLocation] = useState("");
-  // const [link, setLink] = useState("");
-
-  // return (
-  //   <div className="card">
-  //     <form>
-  //       <input id="company_name" type="text" placeholder="Company Name" />
-  //       <input id="job_title" type="text" placeholder="Job Title" />
-  //       <input id="location" type="text" placeholder="Location" />
-  //       <input id="link" type="text" placeholder="Link to job description" />
-  //     </form>
-  //   </div>
-
-  const [username, userInput] = useInput({ type: "text" });
-  const [password, passwordInput] = useInput({ type: "text" });
-
-  return (
-    <>
-      {userInput} : {username} <br />
-      {passwordInput} : {password}
-    </>
-  );
-}
-
-function useInput({ type /*...*/ }) {
+function BuildForm({ id, placeholder }) {
   const [value, setValue] = useState("");
   const input = (
-    <input
-      value={value}
-      onChange={(e) => setValue(e.target.value)}
-      type={type}
-    />
+    <label>
+      {placeholder}
+      <input
+        type="text"
+        id={id}
+        name={id}
+        placeholder={placeholder}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </label>
   );
   return [value, input];
+}
+
+export default function Card() {
+  return (
+    <>
+      <div className="card">
+        <form>
+          <BuildForm id="compname" placeholder="Company Name" />
+          <BuildForm id="jobtitle" placeholder="Job Title" />
+          <BuildForm id="location" placeholder="Location of Office" />
+          <BuildForm id="link" placeholder="Link to application" />
+        </form>
+      </div>
+    </>
+  );
 }
